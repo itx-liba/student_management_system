fd723439ca58e7f3c868c9102c9cf0a4a94e545d
itx-liba <libagill987@gmail.com>
2026-07-21 10:50:48 +0500

    add homeController and route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdmissionController;
@@ -45,10 +46,12 @@ use App\Http\Controllers\Student\DocumentController as StudentPortalDocumentCont
 | Public routes
 |--------------------------------------------------------------------------
 */
+
 Route::get('/', function () {
     return redirect('/login');
 });
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
